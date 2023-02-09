6cc9b981976fa5c7dfdc59a9470ee02b3139fa31
Priyadharshini1710 <priyajeyaraman17@gmail.com>
2023-02-09 10:47:35 +0530

    vinoth's code

--- src/test/java/org/testt/MainTest.java
+++ src/test/java/org/testt/MainTest.java
@@ -1,5 +1,11 @@
 package org.testt;
 
 public class MainTest {
+	public static void main(String[] args) {
+		System.out.println("Test4");
+		System.out.println("Test5");
+		System.out.println("Test6");
+		
+	}
 
 }
