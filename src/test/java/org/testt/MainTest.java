ee179abc1c0fe39216200f7b8e40abc842d67e7c
Priyadharshini1710 <priyajeyaraman17@gmail.com>
2023-02-09 11:46:25 +0530

    vinoth code

--- src/test/java/org/testt/MainTest.java
+++ src/test/java/org/testt/MainTest.java
@@ -2,19 +2,14 @@ package org.testt;
 
 public class MainTest {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
 		System.out.println("Test4");
 		System.out.println("Test5");
 		System.out.println("Test6");
 		
-	}
-
-=======
-		System.out.println("Test1");
-		System.out.println("Test2");
-		System.out.println("Test3");
 		
 	}
 	
->>>>>>> 2ce6e092fcefaf255afe067355325f47a78c1a39
 }
